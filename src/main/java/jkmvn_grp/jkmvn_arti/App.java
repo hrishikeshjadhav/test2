f075442a2cdc6668c4dc3ac1a42c256fd05039c2
hrishikeshjadhav <hrishi.jadhav.study@gmail.com>
2017-04-05 20:15:59 +0530

    adding basic mvn project

---
+++ src/main/java/jkmvn_grp/jkmvn_arti/App.java
@@ -0,0 +1,13 @@
+package jkmvn_grp.jkmvn_arti;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
